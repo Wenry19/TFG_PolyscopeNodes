d444d40dfb4b9f1145a801109d3573d9341d97d4
Wenry19 <enric.lamarca@est.fib.upc.edu>
2021-06-23 02:22:22 +0200

    Block javadoc

--- com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/Block.java
+++ com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/Block.java
@@ -15,27 +15,68 @@ import com.upc.EasyProduction.blocks.operationBlocks.Operation;
 import com.upc.EasyProduction.panelManagement.MainPanel;
 import com.upc.EasyProduction.panelManagement.Workflow;
 
-
+/**
+ * This class represents a block of the workflow.
+ * 
+ * @author Enric Lamarca Ferrés
+ *
+ */
 public class Block extends JLabel{
 	
+	/**
+	 * Name of the block.
+	 */
 	protected String name;
+	/**
+	 * Default code of the block.
+	 */
 	protected String defaultCode;
+	/**
+	 * Code with the correspondent parameters of the block.
+	 */
 	protected String code;
+	/**
+	 * Indentation of the block.
+	 */
 	protected String indentation = "";
-
+	
+	/**
+	 * Parameter panel of the block.
+	 */
 	protected JPanel panel = new JPanel();
+	/**
+	 * Scroll parameter panel of the block.
+	 */
 	protected JScrollPane scroll = new JScrollPane(panel);
 	
+	/**
+	 * Instance of the inner class implemented.
+	 */
 	protected MouseListener mouseListener = new MouseListener();
 	
+	/**
+	 * Width of the label that represents the block.
+	 */
 	protected final int WIDTH = 180;
+	/**
+	 * Height of the label that represents the block.
+	 */
 	protected final int HEIGHT = 40;
 	
+	/**
+	 * Workflow position of the block.
+	 */
 	protected int wfPos = -1;
 	
+	/**
+	 * Boolean that indicates if the block is selected in the workflow.
+	 */
 	protected Boolean isSelected = false;
 		
 	
+	/**
+	 * Constructor.
+	 */
 	public Block() {
 
 		scroll.setBounds(0, 0, 406, 150);		
@@ -56,40 +97,76 @@ public class Block extends JLabel{
 	
 	// getters
 	
+	/**
+	 * Getter of the block's name.
+	 * @return name of the block.
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Getter of the block's default code.
+	 * @return default code of the block.
+	 */
 	public String getDefaultCode() {
 		return defaultCode;
 	}
 	
+	/**
+	 * Generates the code with the correspondent parameters of the block.
+	 * @return code with the correspondent parameters of the block.
+	 */
 	public String generateCode() {
 		code = "";
 		
 		return code;
 	}
 	
+	/**
+	 * Getter of the block's generated code.
+	 * @return code with the correspondent parameters of the block.
+	 */
 	public String getCode() {
 		return generateCode();
 	}
 	
+	/**
+	 * Getter of the block's indentation.
+	 * @return indentation of the block.
+	 */
 	public String getIndentation() {
 		return indentation;
 	}
 	
+	/**
+	 * Getter of the block's workflow position.
+	 * @return workflow position of the block.
+	 */
 	public int getWorkflowPosition() {
 		return wfPos;
 	}
 	
+	/**
+	 * Getter of the BlockData instance of the block.
+	 * @return a new instance of BlockData.
+	 */
 	public BlockData getBlockData() { // EYE!!!!
 		return null;
 	}
 	
+	/**
+	 * Getter of the class name.
+	 * @return class name.
+	 */
 	public String getClassName() {
 		return this.getClass().getName();
 	}
 	
+	/**
+	 * Getter of the block's boolean that indicates if the block is selected or not.
+	 * @return boolean that indicates if the block is selected or not.
+	 */
 	public Boolean getIsSelected() {
 		return isSelected;
 	}
@@ -97,15 +174,26 @@ public class Block extends JLabel{
 	
 	// setters
 	
+	/**
+	 * Setter of the block's workflow position.
+	 * @param pos position of the block in the workflow.
+	 */
 	public void setWorkflowPosition(int pos) {
 		wfPos = pos;
 	}
 	
-
+	/**
+	 * Setter of the block's indentation.
+	 * @param indentation indentation of the block.
+	 */
 	public void setIndentation(String indentation) {
 		this.indentation = indentation;
 	}
 	
+	/**
+	 * Setter of the boolean that indicates if the block is selected in the workflow.
+	 * @param isSelected boolean that indicates if the block is selected in the workflow.
+	 */
 	public void setIsSelected(Boolean isSelected) {
 		
 		this.isSelected = isSelected;
@@ -120,16 +208,25 @@ public class Block extends JLabel{
 		}
 	}
 	
+	/**
+	 * Updates the block's parameters panel to show the correct visualization that corresponds to the current parameters of the block.
+	 */
 	public void setPanel() {}
 	
 	
 	// methods
 	
+	/**
+	 * Selects the block.
+	 */
 	protected void selectBlock() {
 		this.setBorder(BorderFactory.createLineBorder(Color.black, 2));
 		isSelected = true;
 	}
 	
+	/**
+	 * Unselects the block.
+	 */
 	protected void unselectBlock() {
 		this.setBorder(BorderFactory.createLineBorder(Color.gray));
 		isSelected = false;
@@ -138,9 +235,15 @@ public class Block extends JLabel{
 	
 	// inner class
 	
+	/**
+	 * Class that implements a MouseAdapter.
+	 * 
+	 * @author Enric Lamarca Ferrés
+	 *
+	 */
 	protected class MouseListener extends MouseAdapter {
 		
-		
+		@Override
 		public void mouseClicked(MouseEvent e) { // select block
 						
 			Block b = (Block) e.getSource();
@@ -154,6 +257,7 @@ public class Block extends JLabel{
 			
 		}
 		
+		@Override
 		public void mouseDragged(MouseEvent e) { // delete block
 			
 			if (e.getSource() instanceof Operation) {
