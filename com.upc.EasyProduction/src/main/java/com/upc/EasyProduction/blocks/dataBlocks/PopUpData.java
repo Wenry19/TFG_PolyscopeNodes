fa0be1ef6ab9b9acce55451bace063cb50fbd966
Wenry19 <enric.lamarca@est.fib.upc.edu>
2021-06-23 03:22:09 +0200

    popup javadoc

--- com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/dataBlocks/PopUpData.java
+++ com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/dataBlocks/PopUpData.java
@@ -4,18 +4,57 @@ import com.upc.EasyProduction.blocks.Block;
 import com.upc.EasyProduction.blocks.BlockData;
 import com.upc.EasyProduction.blocks.operationBlocks.PopUp;
 
+/**
+ * This class represents the data of the PopUp block.
+ * 
+ * @author Enric Lamarca Ferrés
+ *
+ */
 public class PopUpData extends BlockData{
 	
+	/**
+	 * Indentation of the PopUP block.
+	 */
 	private String indentation;
 	
+	/**
+	 * Title of the PopUp block.
+	 */
 	private String title;
+	/**
+	 * Message of the PopUp block.
+	 */
 	private String message;
 	
+	/**
+	 * boolean that indicates if the PopUp block is a message.
+	 */
 	private boolean isMessage;
+	/**
+	 * boolean that indicates if the PopUp block is a warning.
+	 */
 	private boolean isWarning;
+	/**
+	 * boolean that indicates if the PopUp block is an error.
+	 */
 	private boolean isError;
+	/**
+	 * boolean that indicates if the PopUp block is blocking.
+	 */
 	private boolean isBlocking;
 	
+	/**
+	 * Constructor.
+	 * @param className class name of the PopUp block.
+	 * @param isSelected boolean that indicates if the PopUp block is selected.
+	 * @param indentation indentation of the PopUp block.
+	 * @param title title of the PopUp block.
+	 * @param message message of the PopUp block.
+	 * @param isMessage boolean that indicates if the PopUp block is a message.
+	 * @param isWarning boolean that indicates if the PopUp block is a warning.
+	 * @param isError boolean that indicates if the PopUp block is an error.
+	 * @param isBlocking boolean that indicates if the PopUp block is blocking.
+	 */
 	public PopUpData(String className, Boolean isSelected, String indentation, String title, String message, boolean isMessage, boolean isWarning, boolean isError, boolean isBlocking) {
 		
 		this.className = className;
