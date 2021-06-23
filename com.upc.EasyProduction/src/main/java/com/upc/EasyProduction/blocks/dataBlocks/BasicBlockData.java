6db6dd2bde2506a6c4de815a4201043dd876b390
Wenry19 <enric.lamarca@est.fib.upc.edu>
2021-06-23 02:37:47 +0200

    basic block data javadoc

--- com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/dataBlocks/BasicBlockData.java
+++ com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/dataBlocks/BasicBlockData.java
@@ -2,8 +2,20 @@ package com.upc.EasyProduction.blocks.dataBlocks;
 
 import com.upc.EasyProduction.blocks.BlockData;
 
+/**
+ * This class represents a basic block data which is a block data that contains the common data
+ * that have to be saved of all the blocks.
+ * 
+ * @author Enric Lamarca Ferrés
+ *
+ */
 public class BasicBlockData extends BlockData{
 	
+	/**
+	 * Constructor.
+	 * @param className class name of the corresponding block instance.
+	 * @param isSelected boolean that indicates if the corresponding block is selected.
+	 */
 	public BasicBlockData(String className, Boolean isSelected) {
 		
 		this.className = className;
