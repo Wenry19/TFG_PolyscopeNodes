14a09bce2cd25bc969a5d97c3fd4f9c4943352ae
Wenry19 <enric.lamarca@est.fib.upc.edu>
2021-06-23 03:41:53 +0200

    set outputs javadocs

--- com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/dataBlocks/SetDigitalOutputData.java
+++ com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/dataBlocks/SetDigitalOutputData.java
@@ -4,13 +4,35 @@ import com.upc.EasyProduction.blocks.Block;
 import com.upc.EasyProduction.blocks.BlockData;
 import com.upc.EasyProduction.blocks.operationBlocks.SetDigitalOutput;
 
+/**
+ * This class represents the data of the SetDigitalOutput block.
+ * @author Enric Lamarca Ferrés
+ *
+ */
 public class SetDigitalOutputData extends BlockData{
 	
+	/**
+	 * Indentation of the SetDigitalOutput block.
+	 */
 	private String indentation;
 	
+	/**
+	 * Digital output selected of the SetDigitalOutput block.
+	 */
 	private String out;
+	/**
+	 * Value of the selected output of the SetDigitalOutput block.
+	 */
 	private String value;
 	
+	/**
+	 * Constructor.
+	 * @param className className class name of the SetDigitalOutput block.
+	 * @param isSelected boolean that indicates if the SetDigitalOutput block is selected.
+	 * @param indentation indentation of the SetDigitalOutput block.
+	 * @param out digital output selected of the SetDigitalOutput block.
+	 * @param value value of the selected output of the SetDigitalOutput block.
+	 */
 	public SetDigitalOutputData(String className, Boolean isSelected, String indentation, String out, String value) {
 		
 		this.className = className;
