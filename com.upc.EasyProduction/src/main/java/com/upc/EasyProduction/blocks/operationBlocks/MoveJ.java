f4b7087648f4bb22d0935b41f19bd1ef5a817520
Wenry19 <enric.lamarca@est.fib.upc.edu>
2021-10-11 11:37:47 +0200

    avançant MoveJ, arriba al waypoint però la trajectoria no té sentit

--- com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/operationBlocks/MoveJ.java
+++ com.upc.EasyProduction/src/main/java/com/upc/EasyProduction/blocks/operationBlocks/MoveJ.java
@@ -8,15 +8,25 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
+import com.upc.EasyProduction.blocks.BlockData;
+import com.upc.EasyProduction.blocks.dataBlocks.MoveJData;
 import com.upc.EasyProduction.panelManagement.Workflow;
 import com.ur.urcap.api.domain.userinteraction.RobotPositionCallback2;
+import com.ur.urcap.api.domain.value.jointposition.JointPositions;
 import com.ur.urcap.api.domain.value.robotposition.PositionParameters;
 
 public class MoveJ extends Operation{
 	
 	private JButton setWayPointButton = new JButton("SetWayPoint");
 	
-	private JLabel WayPointLabel = new JLabel("WayPoint: -");
+	private JLabel wayPointLabel = new JLabel("WayPoint: -");
+	
+	private String pos;
+	
+	/**
+	 * Auxiliary boolean that is used to avoid updating the contribution DataModel when it is not the intention.
+	 */
+	private boolean controlUpdateDataModel = true;
 	
 	public MoveJ() {
 		
@@ -54,7 +64,7 @@ public class MoveJ extends Operation{
 		c.gridx = 0;
 		c.gridy = 1;
 		
-		panel.add(WayPointLabel, c);
+		panel.add(wayPointLabel, c);
 		
 		c.gridheight = 1;
 		c.gridwidth = 3;
@@ -66,13 +76,55 @@ public class MoveJ extends Operation{
 		
 	}
 	
+	@Override
+	public String generateCode() {
+		
+		code = "movej(" + pos.replace(" ", ", ") + ")\n"; // afegeixo comes
+		
+		return "\n" + indentation + code;
+		
+	}
+	
+	
+	@Override
+	public BlockData getBlockData() {
+		
+		System.out.print("DEBUG1 -------------------> " + pos + "\n");
+		
+		return new MoveJData(getClassName(), isSelected, indentation, pos);
+	}
+	
+	@Override
+	public void setPanel() {
+		
+		controlUpdateDataModel = false; // per evitar que faci update de la datamodel al fer setValue...
+		
+		wayPointLabel.setText("WayPoint: " + pos);
+
+		controlUpdateDataModel = true;
+	}
+	
+	
+	// setters
+	
+	public void setPos(String pos) {
+		this.pos = pos;
+	}
 	
 	// used to get the robot position
 	private class robotPosManagement extends RobotPositionCallback2{
 
 		@Override
 		public void onOk(PositionParameters parameters) {
-			// TODO Auto-generated method stub
+			
+			
+			pos = parameters.getJointPositions().toString().replace(" rad,", "").replace(" rad", ""); // trec comes perk sembla que donen problemes al serialitzar-les i guardar-les al datamode...
+			
+			wayPointLabel.setText("WayPoint (rad): " + pos.replace(" ", ", "));
+			
+			if (controlUpdateDataModel) {
+				Workflow.getInstance().updateDataModel(new int[] {wfPos});
+			}
 			
 			
 		}
